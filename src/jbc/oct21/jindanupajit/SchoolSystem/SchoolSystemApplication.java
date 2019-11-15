0e292f524ec0409a71cfd6f5e318a73c796ca025
Krissada Jindanupajit <jindanupajit@gmail.com>
2019-11-15 09:11:07 -0500

    Multithreading

--- src/jbc/oct21/jindanupajit/SchoolSystem/SchoolSystemApplication.java
+++ src/jbc/oct21/jindanupajit/SchoolSystem/SchoolSystemApplication.java
@@ -7,9 +7,11 @@ import jbc.oct21.jindanupajit.SchoolSystem.view.Terminal;
 public class SchoolSystemApplication {
     public static void main(String[] args) {
         Thread console = new Thread(new TerminalController(new Terminal( )));
+        console.setName("Console");
         console.start();
 
         Thread telnet = new Thread(new TelnetController());
+        telnet.setName("Telnet");
         telnet.start();
     }
 }
